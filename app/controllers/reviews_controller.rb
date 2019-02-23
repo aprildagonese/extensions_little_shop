63499ee843766c87a9c95977e7ec854655e2bb67
April Dagonese <adagonese@gmail.com>
2019-02-23 16:26:23 -0700

    Adds passing review creation

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,23 @@ class ReviewsController < ApplicationController
     @order_item = OrderItem.find(params[:order_item_id])
     @review = Review.new
   end
+
+  def create
+    @order_item = OrderItem.find(params[:order_item_id])
+    @review = Review.new(review_params)
+    @review.update(user_id: current_user.id, order_item_id: params[:order_item_id])
+    if @review.save
+      flash[:message] = "Your review has been created."
+      redirect_to profile_order_path(@order_item.order)
+    else
+      flash[:alert] = "There was a problem saving your review. Please try again."
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :description, :rating, user_id: current_user.id, order_item_id: params[:order_item_id])
+  end
 end
--- spec/features/user/review_spec.rb
+++ spec/features/user/review_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "as a registered user" do
       visit profile_order_path(order)
 
       within "#oitem-#{order_item.id}" do
-        click_link("Review This Item")
+        click_button("Review This Item")
       end
 
       expect(current_path).to eq(new_order_item_review_path(order_item))
@@ -29,21 +29,21 @@ RSpec.describe "as a registered user" do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       visit new_order_item_review_path(order_item)
-
-      fill_in :title, with: "Great item!"
-      fill_in :description, with: "This item was exactly what I needed."
-      fill_in :rating, with: 5
+      fill_in "Title", with: "Great item!"
+      fill_in "Description", with: "This item was exactly what I needed."
+      fill_in "Rating", with: 5
 
       click_on "Create Review"
-
+      
       review = Review.last
 
       expect(current_path).to eq(profile_order_path(order))
       expect(page).to have_content("Your review has been created.")
-      expect(page).to have_content("Your review: #{review.title}")
+      expect(page).to_not have_button("Review This Item")
+      expect(page).to have_content("Your Review: #{review.title}")
       expect(page).to have_content("#{review.description}")
-      expect(page).to have_content("Your rating: #{review.rating}")
-      expect(page).to have_button("Edit Review")
+      expect(page).to have_content("Your Rating: #{review.rating}")
+      expect(page).to have_button("Edit This Review")
     end
   end
 
