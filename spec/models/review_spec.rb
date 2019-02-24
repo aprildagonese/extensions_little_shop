aa9315de566dc65d971ac1b6b77838a3ba5dedb6
April Dagonese <adagonese@gmail.com>
2019-02-24 11:53:46 -0700

    Adds working .item_reviews and .avg_rating methods

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -26,24 +26,24 @@ RSpec.describe Review, type: :model do
       @order_item2 = create(:fulfilled_order_item, order: @order2, item: @item)
       @order_item3 = create(:fulfilled_order_item, order: @order3, item: @item)
       @order_item4 = create(:fulfilled_order_item, order: @order3, item: @item2)
-      @review = create(:review, user: @user, order_item: @order_item, rating: 1)
-      @review2 = create(:review, user: @user, order_item: @order_item2, rating: 2)
-      @review3 = create(:review, user: @user2, order_item: @order_item3, rating: 3)
-      @review4 = create(:review, user: @user2, order_item: @order_item4, rating: 5)
+      @review = create(:review, user: @user, order_item: @order_item, rating: 1, title: "Review 1")
+      @review2 = create(:review, user: @user, order_item: @order_item2, rating: 2, title: "Review 2")
+      @review3 = create(:review, user: @user2, order_item: @order_item3, rating: 3, title: "Review 3")
+      @review4 = create(:review, user: @user2, order_item: @order_item4, rating: 5, title: "Review 4")
     end
 
     it '.my_reviews' do
       expected1 = [@review, @review2]
       expect(Review.my_reviews(@user)).to eq(expected1)
 
-      expected2 = [@review3]
+      expected2 = [@review3, @review4]
       expect(Review.my_reviews(@user2)).to eq(expected2)
     end
 
     it ".item_reviews" do
       expected1 = [@review, @review2, @review3]
       expected2 = [@review4]
-      
+
       expect(Review.item_reviews(@item)).to eq(expected1)
       expect(Review.item_reviews(@item2)).to eq(expected2)
     end
